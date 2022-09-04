a83d95f8b1bfe57006388bda868ef1039ab037ca
Alan Broadhead <alan.broadhead@laterooms.com>
2022-09-04 14:52:32 +0100

    Add further tests (realsie i got ahead of myself and should have added these before)

--- Tests/CheckoutTests.cs
+++ Tests/CheckoutTests.cs
@@ -5,7 +5,7 @@ public class CheckoutTests
 {
 
     [Fact]
-    public void TotalPriceof2ItemsOfSkuA_Equals50()
+    public void TotalPriceof2ItemsOfSkuA_Equals100()
     { 
         Checkout checkout = new();
         checkout.Scan(new StockKeepingUnitA());
@@ -14,5 +14,37 @@ public class CheckoutTests
 
         Assert.Equal(100, result);
 
+    }
+
+    [Fact]
+    public void TotalPriceOf2SkuAItems_DoesNotEqual200()
+    {
+        Checkout checkout = new();
+        checkout.Scan(new StockKeepingUnitA());
+        checkout.Scan(new StockKeepingUnitA());
+        int result = checkout.GetTotalPrice();
+
+        Assert.NotEqual(200, result);
+    }
+
+    [Fact]
+    public void TotalPriceOf1SkuAItems_DoesNotEqual100()
+    {
+        Checkout checkout = new();
+        checkout.Scan(new StockKeepingUnitA());
+        int result = checkout.GetTotalPrice();
+
+        Assert.NotEqual(100, result);
+    }
+
+
+    [Fact]
+    public void TotalPriceOf1SkuAItems_Equal50()
+    {
+        Checkout checkout = new();
+        checkout.Scan(new StockKeepingUnitA());
+        int result = checkout.GetTotalPrice();
+
+        Assert.Equal(50, result);
     }
 }
